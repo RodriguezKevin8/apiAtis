d613a45689934e323cad90e23291223319bdc0f1
RodriguezKevin8 <kevin.rodriguez.2001m@gmail.com>
2023-11-21 00:45:01 -0600

    se agrega la funcion de busqueda y eliminacion

--- APIS/server/routes/data.routes.js
+++ APIS/server/routes/data.routes.js
@@ -81,6 +81,38 @@ router.get("/detalleshabitacion", async (req, res) => {
   }
 });
 
+router.get("/detalleshabitacion1/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const detallesReservacion = await prisma.habitacion.findFirst({
+      where: {
+        id_habitacion: parseInt(id),
+      },
+      include: {
+        fotos: {
+          select: {
+            foto1: true,
+            foto2: true,
+            foto3: true,
+          },
+        },
+      },
+    });
+
+    if (!detallesReservacion) {
+      return res
+        .status(404)
+        .json({ error: "No se encontraron detalles de la habitación" });
+    }
+
+    res.status(200).json(detallesReservacion);
+  } catch (error) {
+    console.error("Error al obtener los detalles de la habitación:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
 router.get("/detallesreservacion", async (req, res) => {
   try {
     const detallesReservacion = await prisma.habitacion.findMany();
@@ -105,14 +137,20 @@ router.put("/disponibilidad/:id", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-router.get("/fechasEntrada", async (req, res) => {
+
+router.get("/fechasEntrada/:id", async (req, res) => {
+  const { id } = req.params;
   try {
     const fechasEntrada = await prisma.reservaciones.findMany({
+      where: {
+        id_habitacion: parseInt(id),
+      },
       select: {
         fecha_entrada: true,
         fecha_salida: true,
       },
     });
+
     res.status(200).json(fechasEntrada);
   } catch (error) {
     console.error("Error al obtener las fechas de entrada:", error);
@@ -120,4 +158,45 @@ router.get("/fechasEntrada", async (req, res) => {
   }
 });
 
-export default router;  
+router.get("/detallescliente/:comprobante", async (req, res) => {
+  const { comprobante } = req.params;
+  try {
+    const data = await prisma.reservaciones.findMany({
+      where: {
+        comprobante: comprobante,
+      },
+    });
+
+    if (data.length === 0) {
+      return res.status(404).json({
+        error: "No existe una reservación con este número de comprobante",
+      });
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error al obtener datos de la reservación:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
+router.delete("/eliminarReservacion/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const reservacionEliminada = await prisma.reservaciones.delete({
+      where: {
+        id_reservacion: parseInt(id),
+      },
+    });
+
+    res.status(200).json({
+      message: "Reservación eliminada correctamente",
+      data: reservacionEliminada,
+    });
+  } catch (error) {
+    console.error("Error al eliminar la reservación:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
+export default router;
